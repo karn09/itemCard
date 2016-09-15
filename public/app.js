4a43c1bd8f4a20e1001064fb3484fb9edcf7b08d
John Nieves <karn09@gmail.com>
2016-09-14 23:22:20 -0400

    fix closure

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-function (window, Ractive, Kustomer)({
+(function (window, Ractive, Kustomer) {
 
 	// if (!window.$) {
 	//   var jq = document.createElement('script'); jq.type = 'text/javascript';
