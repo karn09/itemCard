44fecd9ec5d4aca454a3426eb01809241f40e5de
John Nieves <karn09@gmail.com>
2016-09-15 00:29:48 -0400

    clean up code, shift item info to take one line only as per spec

--- public/app.js
+++ public/app.js
@@ -1,12 +1,5 @@
 (function (window, Ractive, Kustomer) {
 
-	// if (!window.$) {
-	//   var jq = document.createElement('script'); jq.type = 'text/javascript';
-	//   jq.src = 'http://code.jquery.com/jquery-3.1.0.min.js';
-	//   document.getElementsByTagName('head')[0].appendChild(jq);
-	//   console.log('Hmm.. jQuery not found. Adding to head.')
-	// }
-
 	function updateCard(context) {
 		// cache order object info
 		var orderInfo = context['kobject.order']['attributes']['data'];
@@ -14,21 +7,25 @@
 		var orderTitle = orderInfo['orderTitle'];
 		var orderTotal = orderInfo['orderTotal'];
 
-		var orderedItems = new Ractive({
-			el: '#orderedItemsList',
-			template: '#orderedItemsInfo',
-			data: {
-				"lineItems": lineItems,
-			}
-		});
+		if (lineItems.length === 0) {
+			Kustomer.close();
+		} else {
+			var orderedItems = new Ractive({
+				el: '#orderedItemsList',
+				template: '#orderedItemsInfo',
+				data: {
+					"lineItems": lineItems,
+				}
+			});
 
-		var orderTotal = new Ractive({
-			el: '#orderTotal',
-			template: '#orderTotalInfo',
-			data: {
-				'orderTotal': orderTotal
-			}
-		})
+			var orderTotal = new Ractive({
+				el: '#orderTotal',
+				template: '#orderTotalInfo',
+				data: {
+					'orderTotal': orderTotal,
+				}
+			})
+		}
 	};
 
 	Kustomer.initialize(updateCard),
