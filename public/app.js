b02a1f8ff5f927f523d3d92c8fdabad4e8bfb348
John Nieves <karn09@gmail.com>
2016-09-14 22:42:02 -0400

    html structure, append items

--- public/app.js
+++ public/app.js
@@ -1,8 +1,44 @@
 if (!Kustomer) {
-  throw "Kustomer not initialized"
+  throw "Kustomer SDK not available."
 }
 
-Kustomer.initialize(function(context) {
-  console.log('========CONTEXT=========')
-  console.log(context);
-})
+if (!window.jQuery) {
+  var jq = document.createElement('script'); jq.type = 'text/javascript';
+  jq.src = 'http://code.jquery.com/jquery-3.1.0.min.js';
+  document.getElementsByTagName('head')[0].appendChild(jq);
+  console.log('Hmm.. jQuery not found. Adding to head.')
+}
+
+function updateCard(context) {
+  // cache order object info
+  var orderInfo = context['kobject.order']['data'];
+  var lineItems = orderInfo['lineItems'];
+  var orderTitle = orderInfo['orderTitle'];
+  var orderTotal = orderInfo['orderTotal'];
+
+  var $orderedItemsList = $('#ordered-items');
+
+
+  lineItems.forEach(function(item) {
+    var qty = item.quantity;
+    var price = item.price;
+    var title = item.title;
+    var image = item.image;
+    var newItem =
+    `
+    <div class="item">
+        <img class="ui avatar image" src="` + image + `">
+        <div class="content">
+            <a class="header">` + title + `</a>
+            <div class="description">cost:<span class="item-price">` + price + `</span> - qty:<span class="item-qty">` + quantity + `</span></div>
+        </div>
+    </div>
+    `;
+
+    $orderedItemsList.append($(newItem));
+  });
+
+};
+
+
+Kustomer.initialize(updateCard), Kustomer.on("context", updateCard);
