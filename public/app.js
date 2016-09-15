411e846e3c56b52137da2440320a5275a6bf8e10
John Nieves <karn09@gmail.com>
2016-09-14 23:17:50 -0400

    trying out ractive

--- public/app.js
+++ public/app.js
@@ -1,31 +1,39 @@
-if (!Kustomer) {
-  throw "Kustomer SDK not available."
-}
+function (window, Ractive, Kustomer)({
 
-if (!window.$) {
-  var jq = document.createElement('script'); jq.type = 'text/javascript';
-  jq.src = 'http://code.jquery.com/jquery-3.1.0.min.js';
-  document.getElementsByTagName('head')[0].appendChild(jq);
-  console.log('Hmm.. jQuery not found. Adding to head.')
-}
+	// if (!window.$) {
+	//   var jq = document.createElement('script'); jq.type = 'text/javascript';
+	//   jq.src = 'http://code.jquery.com/jquery-3.1.0.min.js';
+	//   document.getElementsByTagName('head')[0].appendChild(jq);
+	//   console.log('Hmm.. jQuery not found. Adding to head.')
+	// }
 
-function updateCard(context) {
-  // cache order object info
-  var orderInfo = context['kobject.order']['attributes']['data'];
-  var lineItems = orderInfo['lineItems'];
-  var orderTitle = orderInfo['orderTitle'];
-  var orderTotal = orderInfo['orderTotal'];
+	function updateCard(context) {
+		// cache order object info
+		var orderInfo = context['kobject.order']['attributes']['data'];
+		var lineItems = orderInfo['lineItems'];
+		var orderTitle = orderInfo['orderTitle'];
+		var orderTotal = orderInfo['orderTotal'];
 
-  var $orderedItemsList = $('#ordered-items');
+		var $orderedItemsList = $('#ordered-items');
 
+		var orders = new Ractive({
+      el: 'orderedItemsCard',
+      template: '#items-info',
+      data: {
+        "orderInfo": orderInfo,
+        "lineItems": lineItems,
+        "orderTitle": orderTitle,
+        "orderTotal": orderTitle
+      }
+		});
 
-  lineItems.forEach(function(item) {
-    var qty = item.quantity;
-    var price = item.price;
-    var title = item.title;
-    var image = item.image;
-    var newItem =
-    `
+		lineItems.forEach(function (item) {
+			var qty = item.quantity;
+			var price = item.price;
+			var title = item.title;
+			var image = item.image;
+			var newItem =
+				`
     <div class="item">
         <img class="ui avatar image" src="` + image + `">
         <div class="content">
@@ -35,10 +43,13 @@ function updateCard(context) {
     </div>
     `;
 
-    $orderedItemsList.append($(newItem));
-  });
+			$orderedItemsList.append($(newItem));
+		});
 
-};
+	};
 
 
-Kustomer.initialize(updateCard), Kustomer.on("context", updateCard);
+	Kustomer.initialize(updateCard),
+	Kustomer.on("context", updateCard);
+
+})(window, Ractive, Kustomer);
